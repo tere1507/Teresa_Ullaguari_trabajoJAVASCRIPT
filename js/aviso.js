bb8fb583bf518b0fd36feb36842f392db99ef16e
tere1507 <ttullaguari@gmail.com>
2025-03-23 16:23:35 +0100

    update

--- js/aviso.js
+++ js/aviso.js
@@ -5,19 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
     let avisoMostrado = false;//Inicializamos como booleano
 
     boton.addEventListener('click', function(){
-        if(!avisoMostrado){
+        if(!avisoMostrado){ 
             fetch("../json/avisoLegal.json")
             .then(response => response.json())
             .then(data => {
                 textoAviso.innerHTML = `<h2>${data.titulo}</h2>${data.contenido}`;
                 textoAviso.style.display = 'block';
-                avisoMostrado = true;
+                avisoMostrado = 'true';
             })
             .catch(error => {
                 console.error('Error al cargar avisoLegal.json :', error);
                 textoAviso.innerHTML = '<p>No se pudo cargar el aviso legal</p>';
                 textoAviso.style.display = 'block';
-                avisoMostrado = true;
+                avisoMostrado = 'true';
             });
         }else {
             textoAviso.style.display = 'none';
